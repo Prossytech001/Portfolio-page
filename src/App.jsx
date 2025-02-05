5904815c7efd428486f4b42a39736b6e29238435
Prossytech001 <prosperonyebuchi01@gmail.com>
2025-02-05 04:11:51 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import Homepage from './pages/Homepage.jsx';
+import Aboutpage from './pages/Aboutpage.jsx';
+import Education from './pages/Educationpage.jsx';
+import Skillpage from './pages/Skillpage.jsx';
+import Projectpage from './pages/Projectpage.jsx';
+import Contactpage from './pages/Contactpage.jsx';
+
+
+
+import './App.css';
+
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Scrolltop from './components/Scrolltotop.jsx';
+
+ 
+
+function App() {
+ 
+
+  return (
+    <div>
+      <BrowserRouter>
+      <Scrolltop/>
+<Routes>
+<Route path='/' element={<Homepage/>}/>
+
+<Route path='/about' element={<Aboutpage/>}/>
+<Route path='/blog' element={<Education/>}/>
+<Route path='/skill' element={<Skillpage/>}/>
+<Route path='/featuredpost' element={<Projectpage/>}/>
+<Route path='/contact' element={<Contactpage/>}/>
+
+
+
+</Routes>
+
+
+
+
+      </BrowserRouter>
+      
+    
+    </div>
+  )
+}
+
+export default App
